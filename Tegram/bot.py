e453c0f947475c26191d32bcdd30b2e042e2dbbb
zero <mejiacesar03@gmail.com>
2026-07-26 00:10:27 -0500

    add route father folder

--- Tegram/bot.py
+++ Tegram/bot.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ 
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 from Search.my_token import my_token
